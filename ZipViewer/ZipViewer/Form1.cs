3634952d9d86c8f246a9bd136f5c47d390eb6cc4
Dorashiro <dorasiro1@live.jp>
2023-02-11 02:23:48 +0900

    [Commit]リポジトリを作成

---
+++ ZipViewer/ZipViewer/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZipViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var z = ZipFile.Open(@"D:\Dorashiro\デスクトップ\新しいフォルダー (2).zip", ZipArchiveMode.Read, System.Text.Encoding.GetEncoding("Shift_JIS"));
+            foreach (ZipArchiveEntry e in z.Entries)
+            {
+                PictureBox p = new PictureBox();
+                p.Size = new Size(100, 120);
+                using (var img = Image.FromStream(e.Open()))
+                {
+                    p.Image = ThumbMaker.MakeThumb(img, 100, 120);
+                    flowLayoutPanel1.Controls.Add(p);
+                }
+            }
+        }
+    }
+}
---
+++ ZipViewer/ZipViewer/ThumbMaker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipViewer
+{
+    internal class ThumbMaker
+    {
+        /// <summary>
+        /// 指定サイズのサムネを作る
+        /// </summary>
+        /// <param name="img">サムネ化する画像</param>
+        /// <param name="width">幅</param>
+        /// <param name="height">高さ</param>
+        /// <returns></returns>
+        public static Bitmap MakeThumb(Image img, int width, int height)
+        {
+            if(width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("サイズ0以下は無理");
+            }
+
+            // 長い方を基準に縦横比を維持して縮小をかける
+            if(img.Width > img.Height)
+            {
+                height = (int)(img.Height * ((double)width / (double)img.Width));
+            }
+            else
+            {
+                width = (int)(img.Width * ((double)height / (double)img.Height));
+            }
+
+            var bmp = new Bitmap(width, height);
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(img, 0, 0, width, height);
+            }
+                
+            return bmp;
+        }
+    }
+}
